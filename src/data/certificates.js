6abd56e3c7d516edea89a3aac089ab438f4d2fcb
Rafif Ramdhana <rafiframadhana.cs@gmail.com>
2025-04-12 15:22:15 +0700

    Update certifications section

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -1,36 +1,43 @@
-import certification1 from "./../assets/images/certifications/certification-1.png";
-import certification2 from "./../assets/images/certifications/certification-2.png";
-import certification3 from "./../assets/images/certifications/certification-3.png";
-import certification4 from "./../assets/images/certifications/certification-4.png";
-import certification5 from "./../assets/images/certifications/certification-5.png";
+import javascriptFcc from "./../assets/images/certifications/javascript-dsa-fcc.png";
+import webDesignFcc from "./../assets/images/certifications/responsive-web-design-fcc.png";
+import javascriptHr from "./../assets/images/certifications/javascript-hr.png";
+import sqlHr from "./../assets/images/certifications/sql-hr.png";
+import cssHr from "./../assets/images/certifications/css-hr.png";
+import reactHr from "./../assets/images/certifications/react-hr.png";
 
 export const certifications = [
   {
-    img: certification1,
+    img: reactHr,
+    title: "React Certificate",
+    issuer: "Issued by HackerRank",
+    link: "https://www.hackerrank.com/certificates/iframe/932d3848a1cc",
+  },
+  {
+    img: javascriptFcc,
     title: "JavaScript DSA",
     issuer: "Issued by freeCodeCamp",
     link: "https://freecodecamp.org/certification/fccc37dcbb4-6eb7-444e-afc3-a726f368d480/javascript-algorithms-and-data-structures-v8",
   },
   {
-    img: certification2,
+    img: webDesignFcc,
     title: "Responsive Web Design",
     issuer: "Issued by freeCodeCamp",
     link: "https://freecodecamp.org/certification/fccc37dcbb4-6eb7-444e-afc3-a726f368d480/responsive-web-design",
   },
   {
-    img: certification3,
+    img: javascriptHr,
     title: "JavaScript Certificate",
     issuer: "Issued by HackerRank",
     link: "https://www.hackerrank.com/certificates/iframe/63bd5470a0b7",
   },
   {
-    img: certification4,
+    img: sqlHr,
     title: "SQL Certificate",
     issuer: "Issued by HackerRank",
     link: "https://www.hackerrank.com/certificates/iframe/caf7f45d08e6",
   },
   {
-    img: certification5,
+    img: cssHr,
     title: "CSS Certificate",
     issuer: "Issued by HackerRank",
     link: "https://www.hackerrank.com/certificates/iframe/5214ad063526",
